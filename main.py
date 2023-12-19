8ce1943155a57ca23de33d18fa2b1d0e62888aa0
Maruoka Masaharu <kd1305159@st.kobedenshi.ac.jp>
2023-12-19 10:29:35 +0900

    fix_bug

--- main.py
+++ main.py
@@ -11,16 +11,16 @@ with open(filename, encoding='utf8', newline='') as f:
         di["genreId"] = data[i][0]
         di["question"] = data[i][1]
         di["answers"] = [
-            [f"1.{data[i][2]}"],
-            [f"2.{data[i][3]}"],
-            [f"3.{data[i][4]}"],
-            [f"4.{data[i][5]}"]
+            f"1.{data[i][2]}",
+            f"2.{data[i][3]}",
+            f"3.{data[i][4]}",
+            f"4.{data[i][5]}"
         ]
         di["correct"] = data[i][6]
         di["explanation"] = data[i][7]
         di["url"] = data[i][8]
         if i != len(data)-1:
-            print("\t"+str([di]).replace('\'','\"')+',')
+            print("\t"+str(di).replace('\'','\"')+',')
         else:
-            print("\t"+str([di]).replace('\'','\"'))
+            print("\t"+str(di).replace('\'','\"'))
 print("]")
